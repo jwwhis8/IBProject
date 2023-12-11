d426f156c6cd644cb9d636e53b94e35480a80842
hunter <huntersfred@gmail.com>
2023-12-11 14:42:05 -0500

    Added GUI

--- src/readData.py
+++ src/readData.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-# filepath = input("Name of xlsx file: ")
-df = pd.read_excel('sheet.xlsx',header=None)
+def getSheet(path):
+    # filepath = input("Name of xlsx file: ")
+    print(path)
+    global df
+    df = pd.read_excel(path, header=None)
 
 def getName(col):
     name = df.iloc[0, col]  # `iloc` is used for integer-location based indexing
@@ -12,9 +15,3 @@ def getName(col):
 
 def getCourses(col): 
     return df.iloc[1:15, col]  # Slicing from row index 1 to 9, column 'B'
-
-def getYear():
-    return input("Year: (Format: \"20XX-20XX\"): ")
-
-
-print(getName(2))
