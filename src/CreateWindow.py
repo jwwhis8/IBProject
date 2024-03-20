b03dee02a4eea8b6b82d53f34122d055a5a2a799
Hunter <82844928+UnbrokenHunter@users.noreply.github.com>
2024-03-19 20:43:12 -0400

    Fixed Spelling Error

--- src/CreateWindow.py
+++ src/CreateWindow.py
@@ -62,7 +62,7 @@ class App(tk.CTk):
         self.backPageFrame = tk.CTkFrame(master=self)
         self.backPageFrame.grid(row=2, column=0, padx=50, pady=ypad, sticky="nswe")
 
-        self.back_page = tk.CTkLabel(master=self.backPageFrame, text="Select PDF that has the back page",
+        self.back_page = tk.CTkLabel(master=self.backPageFrame, text="Select Back Page PDF",
             font=("helvetica", 20))
         self.back_page.pack(padx=xpad, pady=ypad, anchor="center")
 
