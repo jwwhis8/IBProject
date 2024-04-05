9353088b0f433b0ca911272a5aa80270f475b01f
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-04-05 11:10:39 -0400

    Updated Page

--- src/PageText.py
+++ src/PageText.py
@@ -1,6 +1,6 @@
 
 def formatCourses(courses):
-    list_items = ''.join(f'<li>{course}</li>\n' for course in courses if course == course and course != "nan")
+    list_items = ''.join(f' <p class="c6"><span class="c7">{course}</span></p>\n' for course in courses if course == course and course != "nan")
     return list_items
 
 def getPageText(name, courses, year): 
@@ -8,69 +8,496 @@ def getPageText(name, courses, year):
 <html>
   <head>
     <style>
-      body, p, ul, li {{
-        font-family: "Calibri", Arial, sans-serif;
-        font-size: 11pt;
-        line-height: 1.5; /* Improve readability */
-        word-wrap: break-word;
-        background-color: white;
+            /* latin */
+      @font-face {{
+        font-family: 'Calibri';
+        font-style: normal;
+        font-weight: 400;
+        src: url(https://fonts.gstatic.com/l/font?kit=J7afnpV-BGlaFfdAhLEY6w&skey=a1029226f80653a8&v=v15) format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }}
-      h1 {{
-        font-size: 20pt;
-        margin-bottom: 0.5em; /* Space below headings */
+      /* latin */
+      @font-face {{
+        font-family: 'Calibri';
+        font-style: normal;
+        font-weight: 700;
+        src: url(https://fonts.gstatic.com/l/font?kit=J7aanpV-BGlaFfdAjAo9_pxqHw&skey=cd2dd6afe6bf0eb2&v=v15) format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+0304, U+0308, U+0329, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
       }}
-      h2 {{
-        font-size: 16pt;
-        margin-bottom: 0.4em;
-      }}
-      h3 {{
-        font-size: 14pt;
-        margin-bottom: 0.3em;
-      }}
-      h4 {{
-        font-size: 12pt;
-        margin-bottom: 0.3em;
-      }}
-      b {{
-        font-weight: bold;
-        
-      }}
-      .bold {{
-        font-weight: bold;
-      }}
-      .underline {{
-        text-decoration: underline;
-      }}
-      .center {{
-        text-align: center;
-      }}
-      .left {{
-        text-align: left;
-      }}
-      .padding{{
-        padding-right: 70px;
-        padding-left: 70px;      
-     }}
-     .padding-up{{
-        padding-top: 30px;
-     }}
-      .padding-down{{
-        padding-bottom: 10px;
-     }}
-      .indent {{
-          margin-left: 40px;     
-    }}
-
-      .list-bullet {{
-        list-style-type: square;
-        text-wrap: wrap;
-        margin: 4;
-      }}
-      .list-no-style {{
-        list-style-type: none;
-        padding-left: 4;
-      }}
-
+ul.lst-kix_2a91me569hfb-2 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-7 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_oploub2xta1h-8 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_2a91me569hfb-3 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-8 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_oploub2xta1h-7 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_2a91me569hfb-4 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-5 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_oploub2xta1h-6 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_2a91me569hfb-5 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-6 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_2a91me569hfb-0 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_2a91me569hfb-1 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-0 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-3 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-4 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-1 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_o088gqhwfpjj-2 {{
+    list-style-type: none;
+  }}
+  .lst-kix_o088gqhwfpjj-7 > li:before {{
+    content: "\0025c6   ";
+  }}
+  .lst-kix_o088gqhwfpjj-6 > li:before {{
+    content: "\0025cb   ";
+  }}
+  .lst-kix_o088gqhwfpjj-8 > li:before {{
+    content: "\0025cf   ";
+  }}
+  ul.lst-kix_2a91me569hfb-6 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_2a91me569hfb-7 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_2a91me569hfb-8 {{
+    list-style-type: none;
+  }}
+  .lst-kix_q4urlw6sdrnh-0 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_q4urlw6sdrnh-1 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_q4urlw6sdrnh-2 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_q4urlw6sdrnh-4 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_oploub2xta1h-6 > li:before {{
+    content: "\0025cb   ";
+  }}
+  .lst-kix_q4urlw6sdrnh-3 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_oploub2xta1h-7 > li:before {{
+    content: "\0025c6   ";
+  }}
+  ul.lst-kix_oploub2xta1h-1 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_oploub2xta1h-0 {{
+    list-style-type: none;
+  }}
+  .lst-kix_2a91me569hfb-5 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_q4urlw6sdrnh-6 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_oploub2xta1h-8 > li:before {{
+    content: "\0025cf   ";
+  }}
+  ul.lst-kix_oploub2xta1h-5 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_oploub2xta1h-4 {{
+    list-style-type: none;
+  }}
+  .lst-kix_2a91me569hfb-6 > li:before {{
+    content: "\0025cb   ";
+  }}
+  .lst-kix_2a91me569hfb-7 > li:before {{
+    content: "\0025c6   ";
+  }}
+  ul.lst-kix_oploub2xta1h-3 {{
+    list-style-type: none;
+  }}
+  .lst-kix_q4urlw6sdrnh-5 > li:before {{
+    content: "\0025cf   ";
+  }}
+  ul.lst-kix_oploub2xta1h-2 {{
+    list-style-type: none;
+  }}
+  .lst-kix_2a91me569hfb-8 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_q4urlw6sdrnh-7 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_2a91me569hfb-4 > li:before {{
+    content: "\0025c6   ";
+  }}
+  .lst-kix_q4urlw6sdrnh-8 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_2a91me569hfb-2 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_2a91me569hfb-3 > li:before {{
+    content: "\0025cb   ";
+  }}
+  ul.lst-kix_q4urlw6sdrnh-0 {{
+    list-style-type: none;
+  }}
+  .lst-kix_oploub2xta1h-5 > li:before {{
+    content: "\0025cf   ";
+  }}
+  ul.lst-kix_q4urlw6sdrnh-2 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_q4urlw6sdrnh-1 {{
+    list-style-type: none;
+  }}
+  .lst-kix_2a91me569hfb-0 > li:before {{
+    content: "\002794   ";
+  }}
+  .lst-kix_2a91me569hfb-1 > li:before {{
+    content: "\0025c6   ";
+  }}
+  ul.lst-kix_q4urlw6sdrnh-4 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_q4urlw6sdrnh-3 {{
+    list-style-type: none;
+  }}
+  .lst-kix_oploub2xta1h-4 > li:before {{
+    content: "\0025c6   ";
+  }}
+  ul.lst-kix_q4urlw6sdrnh-6 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_q4urlw6sdrnh-5 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_q4urlw6sdrnh-8 {{
+    list-style-type: none;
+  }}
+  ul.lst-kix_q4urlw6sdrnh-7 {{
+    list-style-type: none;
+  }}
+  .lst-kix_oploub2xta1h-3 > li:before {{
+    content: "\0025cb   ";
+  }}
+  .lst-kix_oploub2xta1h-0 > li:before {{
+    content: "\002794   ";
+  }}
+  .lst-kix_oploub2xta1h-2 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_oploub2xta1h-1 > li:before {{
+    content: "\0025c6   ";
+  }}
+  .lst-kix_o088gqhwfpjj-3 > li:before {{
+    content: "\0025cb   ";
+  }}
+  .lst-kix_o088gqhwfpjj-4 > li:before {{
+    content: "\0025c6   ";
+  }}
+  .lst-kix_o088gqhwfpjj-5 > li:before {{
+    content: "\0025cf   ";
+  }}
+  .lst-kix_o088gqhwfpjj-1 > li:before {{
+    content: "\0025c6   ";
+  }}
+  .lst-kix_o088gqhwfpjj-2 > li:before {{
+    content: "\0025cf   ";
+  }}
+  li.li-bullet-0:before {{
+    margin-left: -18pt;
+    white-space: nowrap;
+    display: inline-block;
+    min-width: 18pt;
+  }}
+  .lst-kix_o088gqhwfpjj-0 > li:before {{
+    content: "\002794   ";
+  }}
+  ol {{
+    margin: 0;
+    padding: 0;
+  }}
+  table td,
+  table th {{
+    padding: 0;
+  }}
+  .c4 {{
+    margin-left: 36pt;
+    padding-top: 0pt;
+    padding-left: 0pt;
+    padding-bottom: 0pt;
+    line-height: 1.15;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  .c7 {{
+    color: #000000;
+    font-weight: 400;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-size: 11pt;
+    font-family: "Calibri";
+    font-style: normal;
+  }}
+  .c17 {{
+    margin-left: 36pt;
+    padding-top: 0pt;
+    padding-bottom: 0pt;
+    line-height: 1.15;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  .c2 {{
+    color: #000000;
+    font-weight: 700;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-size: 14pt;
+    font-family: "Calibri";
+    font-style: normal;
+  }}
+  .c13 {{
+    color: #000000;
+    font-weight: 400;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-size: 11pt;
+    font-family: "Arial";
+    font-style: normal;
+  }}
+  .c5 {{
+    -webkit-text-decoration-skip: none;
+    color: #000000;
+    text-decoration: underline;
+    vertical-align: baseline;
+    text-decoration-skip-ink: none;
+    font-style: normal;
+  }}
+  .c18 {{
+    padding-top: 0pt;
+    padding-bottom: 0pt;
+    line-height: 1.15;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  .c6 {{
+    padding-top: 0pt;
+    padding-bottom: 0pt;
+    line-height: 1;
+    orphans: 2;
+    widows: 2;
+    text-align: center;
+  }}
+  .c1 {{
+    padding-top: 0pt;
+    padding-bottom: 0pt;
+    line-height: 1;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  .c15 {{
+    padding-top: 0pt;
+    padding-bottom: 0pt;
+    line-height: 1.15;
+    orphans: 2;
+    widows: 2;
+    text-align: center;
+  }}
+  .c14 {{
+    color: #000000;
+    text-decoration: none;
+    vertical-align: baseline;
+    font-style: normal;
+  }}
+  .c19 {{
+    background-color: #ffffff;
+    max-width: 540pt;
+    padding: 36pt 36pt 36pt 36pt;
+  }}
+  .c3 {{
+    font-size: 14pt;
+    font-family: "Calibri";
+    font-weight: 700;
+  }}
+  .c9 {{
+    background-color: #ffffff;
+    font-family: "Calibri";
+    font-weight: 400;
+  }}
+  .c10 {{
+    font-size: 14pt;
+    font-family: "Calibri";
+    font-weight: 400;
+  }}
+  .c0 {{
+    padding: 0;
+    margin: 0;
+  }}
+  .c8 {{
+    font-weight: 700;
+    font-family: "Calibri";
+  }}
+  .c16 {{
+    font-weight: 400;
+    font-family: "Calibri";
+  }}
+  .c21 {{
+    font-size: 8pt;
+  }}
+  .c12 {{
+    font-style: italic;
+  }}
+  .c11 {{
+    height: 11pt;
+  }}
+  .c20 {{
+    font-size: 11pt;
+  }}
+  .title {{
+    padding-top: 0pt;
+    color: #000000;
+    font-size: 26pt;
+    padding-bottom: 3pt;
+    font-family: "Arial";
+    line-height: 1.15;
+    page-break-after: avoid;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  .subtitle {{
+    padding-top: 0pt;
+    color: #666666;
+    font-size: 15pt;
+    padding-bottom: 16pt;
+    font-family: "Arial";
+    line-height: 1.15;
+    page-break-after: avoid;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  li {{
+    color: #000000;
+    font-size: 11pt;
+    font-family: "Arial";
+  }}
+  p {{
+    margin: 0;
+    color: #000000;
+    font-size: 11pt;
+    font-family: "Arial";
+  }}
+  h1 {{
+    padding-top: 20pt;
+    color: #000000;
+    font-size: 20pt;
+    padding-bottom: 6pt;
+    font-family: "Arial";
+    line-height: 1.15;
+    page-break-after: avoid;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  h2 {{
+    padding-top: 18pt;
+    color: #000000;
+    font-size: 16pt;
+    padding-bottom: 6pt;
+    font-family: "Arial";
+    line-height: 1.15;
+    page-break-after: avoid;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  h3 {{
+    padding-top: 16pt;
+    color: #434343;
+    font-size: 14pt;
+    padding-bottom: 4pt;
+    font-family: "Arial";
+    line-height: 1.15;
+    page-break-after: avoid;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  h4 {{
+    padding-top: 14pt;
+    color: #666666;
+    font-size: 12pt;
+    padding-bottom: 4pt;
+    font-family: "Arial";
+    line-height: 1.15;
+    page-break-after: avoid;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  h5 {{
+    padding-top: 12pt;
+    color: #666666;
+    font-size: 11pt;
+    padding-bottom: 4pt;
+    font-family: "Arial";
+    line-height: 1.15;
+    page-break-after: avoid;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
+  h6 {{
+    padding-top: 12pt;
+    color: #666666;
+    font-size: 11pt;
+    padding-bottom: 4pt;
+    font-family: "Arial";
+    line-height: 1.15;
+    page-break-after: avoid;
+    font-style: italic;
+    orphans: 2;
+    widows: 2;
+    text-align: left;
+  }}
     </style>
   </head>
   <body class="c19 doc-content">
@@ -147,9 +574,10 @@ def getPageText(name, courses, year):
         the relevant department to consider your request)</span
       >
     </p>
-    <h3 class="center bold underline">Course Recommendations</h3>
-    <h4 class="center underline bold">{name}</h4>
     <ul class="center list-no-style">
+    <p class="c6"><span class="c3 c5">Course Recommendations</span></p>
+    <p class="c6 c11"><span class="c5 c3"></span></p>
+    <p class="c6"><span class="c5 c3"> {name} </span></p>
         {formatCourses(courses)}
     </ul>
     <p class="c1 c11"><span class="c7"></span></p>
@@ -203,7 +631,7 @@ def getPageText(name, courses, year):
     <p class="c1">
       <span class="c8">Notes from Advisor/College Counselor:</span
       ><span class="c7"
-        >&nbsp;_________________________________________________________________</span
+        >&nbsp;________________________________________________________________</span
       >
     </p>
     <p class="c1">
