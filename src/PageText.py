9467f2da0ab1e0bce273758d9eca8fc6ef41bb1f
Hunter Laptop <82844928+UnbrokenHunter@users.noreply.github.com>
2024-04-05 10:17:20 -0400

    Moved Wkhtmltopdf into the lib folder

--- src/PageText.py
+++ src/PageText.py
@@ -73,35 +73,162 @@ def getPageText(name, courses, year):
 
     </style>
   </head>
-  <body>
-    <h1 class="center bold padding-up">{year} COURSE RECOMMENDATIONS</h1>
-    <h2 class="left padding">Considerations as you review your course recommendations:</h2>
-    <ul class="list-bullet padding">
-      <li class="indent">Your teachers considered many factors when determining their recommendations for you. If you have any questions, talk to your current teacher in that department.</li>
-      <li class="indent">Your recommended courses are subject to change if your academic work changes significantly.</li>
-      <li class="indent">You do not need to be recommended for most elective courses, but some have prerequisites.</li>
+  <body class="c19 doc-content">
+    <p class="c15">
+      <span class="c3">2024-2025 </span
+      ><span class="c3">COURSE RECOMMENDATIONS</span>
+    </p>
+    <p class="c11 c18"><span class="c13"></span></p>
+    <p class="c18">
+      <span class="c10">Considerations as you </span
+      ><span class="c3">review your course recommendations</span
+      ><span class="c14 c10">:</span>
+    </p>
+    <ul class="c0 lst-kix_2a91me569hfb-0 start">
+      <li class="c4 li-bullet-0">
+        <span class="c7"
+          >your teachers considered many factors when determining their
+          recommendations for you and we encourage you to follow their
+          recommendations. If you have any questions about your recommendation,
+          we encourage you to talk to your current teacher in that
+          department.</span
+        >
+      </li>
+      <li class="c4 li-bullet-0">
+        <span class="c16">your recommended courses are subject to cha</span
+        ><span class="c16">nge </span
+        ><span class="c9"
+          >if your academic work for the last part of the school year changes
+          significantly</span
+        >
+      </li>
+      <li class="c4 li-bullet-0">
+        <span class="c7"
+          >you do not need to be recommended for most elective courses (but some
+          have prerequisites)</span
+        >
+      </li>
     </ul>
-    <h2 class="left padding">Considerations as you request courses:</h2>
-    <ul class="list-bullet padding">
-      <li class="indent">Consider your overall load, not just each individual course.</li>
-      <li class="indent">Balance and perspective in your life is important, but also challenge yourself appropriately.</li>
-      <li class="indent">Select coursework that engages you intellectually and meets your goals for college and beyond. Colleges expect you to challenge yourself while maintaining strong grades.</li>
-      <li class="indent"><b>Consider the total number of Honors, AP, and IB courses you request.</b></li>
+    <p class="c18">
+      <span class="c10">Considerations as you </span
+      ><span class="c3">request courses</span><span class="c10 c14">:</span>
+    </p>
+    <ul class="c0 lst-kix_2a91me569hfb-0">
+      <li class="c4 li-bullet-0">
+        <span class="c7"
+          >consider not just each individual course, but your overall load</span
+        >
+      </li>
+      <li class="c4 li-bullet-0">
+        <span class="c7"
+          >try to have a sense of balance and perspective in your life, but also
+          appropriately challenge yourself</span
+        >
+      </li>
+      <li class="c4 li-bullet-0">
+        <span class="c7"
+          >select coursework that will engage you intellectually as well as
+          position you to meet your goals for college matriculation and beyond.
+          Typically, colleges are expecting that you have challenged yourself
+          with the curriculum available to you while still maintaining strong
+          grades.</span
+        >
+      </li>
+      <li class="c4 li-bullet-0">
+        <span class="c14 c8 c20"
+          >seriously consider the total number of Honors, AP, and IB courses you
+          request</span
+        >
+      </li>
     </ul>
-    <p class="left padding">(Requesting a course for which you were NOT recommended will trigger the relevant department to consider your request.)</p>
-    <h3 class="center bold underline">Course Recommendations</h3>
-    <h4 class="center underline bold">{name}</h4>
-    <ul class="center list-no-style">
-        {formatCourses(courses)}
-    </ul>
-    <p class="padding">I, _________________________________, confirm that I have reviewed my course recommendations for 2023-2024 and requested courses while considering all of the above information.</p>
-    <p class="padding padding-down"><b>Student Signature: </b>_________________________________________</p>
-    <p class="padding">I confirm that I have reviewed my child's course recommendations for 2023-2024 and I approve the courses requested while considering all of the above information.</p>
-    <p class="padding padding-down"><b>Parent Signature: </b>______________________________________________</p>
-    <p class="padding">I confirm that I have reviewed my advisee's course recommendations for 2023-2024 and I approve the courses requested while considering all of the above information.</p>
-    <p class="padding padding-down"><b>Advisor Signature: </b>______________________________________________</p>
-    <p class="padding">I confirm that I have reviewed my counselee's course recommendations for 2023-2024 and I approve the courses requested while considering all of the above information.</p>
-    <p class="padding"><b>College Counselor Signature (for rising 12th grade only): </b>________________________________________</p>
+    <p class="c17">
+      <span class="c16"
+        >(requesting a course for which you were NOT recommended will trigger
+        the relevant department to consider your request)</span
+      >
+    </p>
+    <p class="c6"><span class="c3 c5">Course Recommendations</span></p>
+    <p class="c6 c11"><span class="c5 c3"></span></p>
+    <p class="c6"><span class="c5 c3">Student x</span></p>
+    <p class="c6"><span class="c7">English III</span></p>
+    <p class="c6"><span class="c7">Pre-Calculus</span></p>
+    <p class="c6"><span class="c7">Physics</span></p>
+    <p class="c6"><span class="c7">IB German A Lit HL Yr 1</span></p>
+    <p class="c6"><span class="c7">US Gov&#39;t/Econ</span></p>
+    <p class="c6"><span class="c7">TOK I</span></p>
+    <p class="c6"><span class="c7">Ceramics II</span></p>
+    <p class="c6"><span class="c7">Dig Photo I</span></p>
+    <p class="c6"><span class="c7">Fun Art</span></p>
+    <p class="c6"><span class="c7">Sculpture</span></p>
+    <p class="c1 c11"><span class="c7"></span></p>
+    <p class="c1 c11"><span class="c14 c16 c21"></span></p>
+    <p class="c1">
+      <span class="c7"
+        >I, _________________________________, confirm that I have reviewed my
+        course recommendations for 2024-2025 and requested courses (on opposite
+        side of this paper) while considering all of the above
+        information.</span
+      >
+    </p>
+    <p class="c1">
+      <span class="c14 c8 c20"
+        >Student Signature: _________________________________________</span
+      >
+    </p>
+    <p class="c1 c11"><span class="c7"></span></p>
+    <p class="c1">
+      <span class="c7"
+        >I confirm that I have reviewed my child&rsquo;s course recommendations
+        for 2024-2025 and I approve the courses requested (on opposite side of
+        this paper) while considering all of the above information.</span
+      >
+    </p>
+    <p class="c1">
+      <span class="c8">Parent Signature: </span
+      ><span class="c7">_____________________________________________</span>
+    </p>
+    <p class="c1 c11"><span class="c7"></span></p>
+    <p class="c1">
+      <span class="c7"
+        >I confirm that I have reviewed my advisee&rsquo;s/counselee&rsquo;s
+        course recommendations for 2024-2025 and I approve the courses requested
+        (on opposite side of this paper) while considering all of the above
+        information.</span
+      >
+    </p>
+    <p class="c1">
+      <span class="c8">Advisor Signature:</span
+      ><span class="c7"
+        >&nbsp;_____________________________________________</span
+      >
+    </p>
+    <p class="c1">
+      <span class="c8">College Counselor Signature </span
+      ><span class="c8 c12">(for rising 12th grade only)</span
+      ><span class="c8">: </span
+      ><span class="c7">______________________________________</span>
+    </p>
+    <p class="c1">
+      <span class="c8">Notes from Advisor/College Counselor:</span
+      ><span class="c7"
+        >&nbsp;_________________________________________________________________</span
+      >
+    </p>
+    <p class="c1">
+      <span class="c7"
+        >_________________________________________________________________________________________________</span
+      >
+    </p>
+    <p class="c1">
+      <span class="c7"
+        >_________________________________________________________________________________________________</span
+      >
+    </p>
+    <p class="c1">
+      <span class="c16"
+        >_________________________________________________________________________________________________</span
+      >
+    </p>
   </body>
 </html>
 """)
--- src/createPDF.py
+++ src/createPDF.py
@@ -7,8 +7,19 @@ from PageText import getPageText
 def create_individual_pdf(name, courses, year, filename):
     # Use your getPageText function to generate HTML content for each student
     html_content = getPageText(name, courses, year)
+
+    # Config    
+    # Assuming wkhtmltopdf.exe is in "..\wkhtmltopdf\bin\" relative to this script's location
+    base_dir = os.path.dirname(os.path.realpath(__file__))  # Gets the directory where the script is located
+    wkhtmltopdf_path = os.path.join(base_dir, "..", "wkhtmltopdf", "bin", "wkhtmltopdf.exe")
+
+    # Make the path absolute to avoid relative path issues
+    wkhtmltopdf_path = os.path.abspath(wkhtmltopdf_path)
+
+    config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf_path)
+
     # Generate a PDF from this HTML content
-    pdfkit.from_string(html_content, filename)
+    pdfkit.from_string(html_content, filename, configuration=config)
 
 def create_and_combine_pdfs(output_pdf, sheet_path, year):
     GetSheet.getSheet(sheet_path)  
